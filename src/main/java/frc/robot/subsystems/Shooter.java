693c019da6d24a99e5bc6a1a1e22846773f28162
JPfeiffer87 <accordionchan@gmail.com>
2022-03-17 18:37:18 -0600

    Shooter class

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//REV imports
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+//Robot imports
+import frc.robot.Constants;
+import frc.robot.Constants.motorConstants;
+
+public class Shooter extends SubsystemBase {
+  public CANSparkMax shooterMot1,shooterMot2, indexMot;
+  public Shooter() {
+    shooterMot1 = new CANSparkMax(motorConstants.SPEED_CONT16, MotorType.kBrushless);
+    shooterMot2 = new CANSparkMax(motorConstants.SPEED_CONT17, MotorType.kBrushless);
+
+    indexMot = new CANSparkMax(motorConstants.SPEED_CONT18, MotorType.kBrushless);
+
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void runIndex (double speed) {
+    indexMot.set(speed);
+  }
+}
